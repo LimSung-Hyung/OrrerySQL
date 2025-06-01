bb680add27f3a9c542a150b94eb2fa7649255d73
LimSung-Hyung <dbwjdtjdgud1@gmail.com>
2025-06-01 21:49:44 +0900

    Delete SQLCoreProject directory

--- SQLCoreProject/data/Key_loader.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def load_key():
-    path = os.path.join(os.getenv("USERPROFILE") or os.getenv("HOME"), ".tns_key")
-    return open(path, "rb").read().ljust(32, b'0')  # 키가 짧으면 오른쪽 0으로 채움
--- SQLCoreProject/plugin/duckdb_plugin.py
+++
@@ -1,7 +0,0 @@
-class DuckDBPlugin:
-    def __init__(self):
-        self.conn = None
-    def connect(self, file_path):
-        import duckdb
-        self.conn = duckdb.connect(file_path)
-        # 추가: 테이블 목록, 쿼리 등 메서드 구현 가능 
